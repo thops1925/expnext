1df24b5e28e2e750a70b1ac8f17951623e3d425d
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-03-03 22:51:18 +0800

    movie components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-"use client";
-
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
+  const res = await data.json();
+  console.log(res);
   return (
     <main>
       <h1 className="">Hello Next</h1>
+      <div>
+        {res.results.map((movie: any) => (
+          <div>
+            <h1>{movie.title}</h1>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
